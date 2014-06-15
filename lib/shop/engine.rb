6bff5574365123bbad28531ccd694230877223c6
wangfuhai <wangfuhai@gmail.com>
2014-06-15 17:50:07 +0800

    增加类扩展支持

--- lib/shop/engine.rb
+++ lib/shop/engine.rb
@@ -1,5 +1,10 @@
 module Shop
   class Engine < ::Rails::Engine
     isolate_namespace Shop
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*.rb").each do |c|        
+        require_dependency(c)
+      end
+    end
   end
 end
